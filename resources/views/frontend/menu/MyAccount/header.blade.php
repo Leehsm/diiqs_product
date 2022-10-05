726e240c557a5e14da37756c05771cf9f7ecca11
Leehsm <shahzleehasimwork@gmail.com>
2022-10-05 11:36:00 +0800

    header for my account menu

---
+++ resources/views/frontend/menu/MyAccount/header.blade.php
@@ -0,0 +1,9 @@
+<table class="table table-hover">
+    <thead>
+        <th><a href="#" class="form-group my-account-a"></a></th>
+        <th><a href="/dashboard" class="form-group my-account-a">Dashboard</a></th>
+        <th><a href="/orders" class="form-group my-account-a">Orders</a></th>
+        <th><a href="/account/detail" class="form-group my-account-a">Account Details</a></th>
+        <th><a href="#" class="form-group my-account-a">Logout</a></th>
+    </thead>
+</table>
