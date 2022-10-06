e036f4b72b694974724239bc433b1e5ceb056521
Leehsm <shahzleehasimwork@gmail.com>
2022-10-06 09:40:40 +0800

    design footer

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -1,6 +1,30 @@
 <div style="clear: both" class="panel panel-default footer">
-    <div class="panel-body">
-      Panel content
-    </div>
-    <div class="panel-footer">Panel footer</div>
+  <div class="container fot">   
+      <h5>
+          <a href="#">Contact Us</a> 
+          <a href="#">Term & Condition</a> 
+          <a href="#">Track Your Order</a>
+      </h5>  
+  </div>  <br>
+  <footer class="footer-section"> 
+      <div class="copyright-area">  
+          <div class="container">  
+              <div class="row">  
+                  <div class="col-xl-3 col-lg-3 text-center text-lg-left">  
+                      <div class="copyright-text">  
+                          <p> Copyright © 2022, DIIQS BEAUTY </p>  
+                      </div>  
+                  </div>  
+                  <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">  
+                      <div class="footer-menu">  
+                        <img class="footer-icon" src="{{ url('/upload/visa.png') }}">
+                        <img class="footer-icon" src="{{ url('/upload/mastercard.png') }}">  
+                        <img class="footer-icon" src="{{ url('/upload/bank.png') }}">  
+                      </div>  
+                  </div>  
+              </div>  
+          </div>  
+      </div>
+  </footer>
 </div>
+
