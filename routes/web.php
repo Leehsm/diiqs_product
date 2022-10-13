c9ba8d0cd3c3b1c751e63dff8f43c4976a4cc6fb
Leehsm <shahzleehasimwork@gmail.com>
2022-10-13 13:40:49 +0800

    Admin page route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,7 +24,9 @@ Route::get('/logout', function () {
     return redirect('/login');
 });
 
+// Route::view('/register','register');
 Route::post("/login",[UserController::class,'login']);
+Route::get("/register",[UserController::class,'register']);
 Route::get("/",[ProductController::class,'index']);
 Route::get("detail/{id}",[ProductController::class,'detail']);
 Route::post("add_to_cart",[ProductController::class,'addToCart']);
@@ -44,3 +47,34 @@ Route::get('feedback', [ProductController::class, 'feedback']);
 Route::get('dashboard', [AccountController::class, 'dashboard']); 
 Route::get('order/history', [AccountController::class, 'myAccount']); 
 Route::get('account/detail', [AccountController::class, 'accDetails']); 
+
+
+
+//ADMIN SIDE
+Route::get('admin/dashboard', [AdminController::class, 'dashboard']);
+
+//Product
+Route::get('product', [AdminController::class, 'viewProduct']);
+Route::get('product/add', [AdminController::class, 'addProduct']);
+Route::post('product/store', [AdminController::class, 'storeProduct']);
+// Route::get('product/edit/{id}', [AdminController::class, 'editProduct']);
+Route::get('product/delete/{id}', [AdminController::class, 'deleteProduct']);
+
+//Combo
+Route::get('combo', [AdminController::class, 'viewCombo']);
+Route::get('combo/add', [AdminController::class, 'addCombo']);
+// Route::get('combo/edit/{id}', [AdminController::class, 'editCombo']);
+// Route::get('combo/delete/{id}', [AdminController::class, 'deleteCombo']);
+
+//Slider
+Route::get('slider', [AdminController::class, 'viewSlider']);
+Route::get('slider/add', [AdminController::class, 'addSlider']);
+// Route::get('slider/edit/{id}', [AdminController::class, 'editSlider']);
+// Route::get('slider/delete/{id}', [AdminController::class, 'deleteSlider']);
+
+//Order
+Route::get('order', [AdminController::class, 'viewOrder']);
+
+
+//Feedback
+Route::get('feedback', [AdminController::class, 'viewFeedback']);
