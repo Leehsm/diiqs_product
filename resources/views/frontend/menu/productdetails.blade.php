20b36b9b69c457de5683d1e1d7dd7b0c44295963
Leehsm <shahzleehasimwork@gmail.com>
2022-10-13 09:11:22 +0800

    alter css for product detail

--- resources/views/frontend/menu/productdetails.blade.php
+++ resources/views/frontend/menu/productdetails.blade.php
@@ -1,22 +1,22 @@
 @extends('master')
 @section('content')
-<div class="container">
-    <div class="row">
-        <a href="/">< Go Back</a>
-        <div class="col-sm-6">
-            <img class="detail-img" src="{{asset($product['gallery'])}}" alt="">
-        </div>
-        <div class="col-sm-6">
-            <h2></h2>
-            <h2>{{$product['name']}}</h2>
-            <h3>Price :RM {{$product['price']}}</h3>
-            <h4>Details: {{$product['description']}}</h4>
-            <form action="/add_to_cart" method="POST">
-                @csrf
-                <input class="form-control prodQty" type="number" id="quantity" name="quantity" min="1" max="{{ $product['quantity'] }}" value="1" required>
-                <input type="hidden" name="product_id" value={{ $product['id'] }}>
-                <button class="btn btn-primary addToCart-btn"> Add To Cart </button>
-                
+<div style="text-align: center;" class="container">
+    <div class="product-wrapper" id="aboutProduct">
+        <h3> {{$product['name']}} </h3>
+        <div class="our-product">
+            <div class="about-us">
+                <img class="about-detail-img " src="{{asset($product['gallery'])}}">
+            </div>
+            <div style=" width:50%;float: right;">
+                <h4>Price: RM {{$product['price']}}</h4>
+                <a style="color: black">Description: {{$product['description']}}</a>
+                <form action="/add_to_cart" method="POST">
+                    @csrf
+                    <input style="width: 50%; margin: auto;" class="form-control prodQty" type="number" id="quantity" name="quantity" min="1" max="{{ $product['quantity'] }}" value="1" required>
+                    <input type="hidden" name="product_id" value={{ $product['id'] }}>
+                    <button class="btn btn-primary addToCart-btn"> Add To Cart </button>
+                </form>
+            </div>
         </div>
     </div>
     
